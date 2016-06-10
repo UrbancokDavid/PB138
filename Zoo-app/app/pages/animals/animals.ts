36c6088886d8feb56d9b4dc94ef626bd03ffeb51
Ondrej Mular <omular@redhat.com>
2016-06-11 01:24:52 +0200

    app: add serachbar for animals

--- Zoo-app/app/pages/animals/animals.ts
+++ Zoo-app/app/pages/animals/animals.ts
@@ -1,6 +1,7 @@
 import {NavController, Page, Toast} from 'ionic-angular';
 import {GeneralProvider} from '../../providers/general-provider';
-import {Settings} from '../../common/settings'
+import {Settings} from '../../common/settings';
+import {Tools} from '../../common/tools';
 
 
 @Page({
@@ -8,7 +9,10 @@ import {Settings} from '../../common/settings'
 })
 export class Animals {
   animals = [];
+  filteredAnimals = [];
   settings = Settings;
+  searchBarVisible: boolean = false;
+  searchString: string = '';
 
   constructor(
     private nav: NavController,
@@ -17,13 +21,40 @@ export class Animals {
     this.doRefresh();
   }
 
+  showSearchBar() {
+    this.searchBarVisible = true;
+  }
+
+  hideSearchBar() {
+    this.searchBarVisible = false;
+  }
+
+  filterAnimals() {
+    let str = this.searchString.trim().toLocaleLowerCase();
+    console.log("search");
+    if (str == '') {
+      this.filteredAnimals = this.animals;
+    }
+    console.log("query: " + str);
+
+    this.filteredAnimals = this.animals.filter(item => {
+      return (item['spieces']['en'].toLocaleLowerCase().indexOf(str) > -1);
+    });
+  }
+
+  onCancel() {
+    console.log("searchbar cancel");
+  }
+
   doRefresh(force: boolean = false) {
     console.log("refreshing...");
     this.generalProvider.getAllAnimals(force).then(animals => {
       this.animals = animals;
+      this.filterAnimals();
       console.log("done");
-    }).catch(() => {
+    }).catch((error) => {
       console.log("failed");
+      console.log(error);
       let toast = Toast.create({
         message: 'Unable to connect to server.',
         showCloseButton: true,
