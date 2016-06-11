908cea3fe09bc7e97b0bc4233a4638c50143709b
Ondrej Mular <omular@redhat.com>
2016-06-11 10:24:51 +0200

    app: add sound

--- Zoo-app/app/pages/animaldetails/animaldetails.ts
+++ Zoo-app/app/pages/animaldetails/animaldetails.ts
@@ -3,14 +3,11 @@ import {Settings} from '../../common/settings';
 import {TranslateService, TranslatePipe} from 'ng2-translate/ng2-translate';
 
 
-declare var Media:any;
-
 @Page({
   templateUrl: 'build/pages/animaldetails/animaldetails.html',
   pipes: [TranslatePipe]
 })
 export class AnimaldetailsPage {
-  soundObj;
   private item;
   settings = Settings;
 
@@ -24,19 +21,5 @@ export class AnimaldetailsPage {
     this.item = this.navParams.data;
   }
 
-  playSound(file) {
-    var src = Settings.host + file;
-    this.soundObj = new Media(src);
 
-    this.soundObj.play();
-  }
-
-  stopSound() {
-    if (this.soundObj) {
-      this.soundObj.pause();
-      this.soundObj.stop();
-      this.soundObj.release();
-      this.soundObj = null;
-    }
-  }
 }
--- Zoo-app/app/pages/animalinfo/animalinfo.ts
+++ Zoo-app/app/pages/animalinfo/animalinfo.ts
@@ -2,11 +2,16 @@ import {Page, NavController, NavParams} from 'ionic-angular';
 import {Settings} from '../../common/settings';
 import {TranslateService, TranslatePipe} from 'ng2-translate/ng2-translate';
 
+
+declare var Media:any;
+
 @Page({
   templateUrl: 'build/pages/animalinfo/animalinfo.html',
   pipes: [TranslatePipe]
 })
 export class AnimalinfoPage {
+  soundObj;
+  showPlay: boolean = true;
   private item;
   settings = Settings;
 
@@ -18,5 +23,22 @@ export class AnimalinfoPage {
     this.nav = nav;
     this.navParams = navParams;
     this.item = this.navParams.data;
+  }
+
+  playSound(file) {
+    var src = Settings.host + file;
+    this.soundObj = new Media(src);
+    this.soundObj.play();
+    this.showPlay = false;
+  }
+
+  stopSound() {
+    if (this.soundObj) {
+      this.soundObj.pause();
+      this.soundObj.stop();
+      this.soundObj.release();
+      this.soundObj = null;
+      this.showPlay = true;
+    }
   }
 }
